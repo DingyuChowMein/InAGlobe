82d4f8936383241e5124e372b75767e6df8b9ace
rohit <rohit11may@gmail.com>
2019-10-12 17:06:39 +0100

    Add flask server logging to see problem in heroku logs

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 import os
+import logging
 from flask import Flask, render_template
 
 app = Flask(__name__, static_folder='build/static', template_folder='build')
 port = int(os.environ.get("PORT", 5000))
 
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 @app.route("/")
 def hello():
     return render_template('index.html')
