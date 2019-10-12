9a71c2ea187b4eddcb5ffae30618c0cd7b3f6c12
rohit <rohit11may@gmail.com>
2019-10-12 16:59:33 +0100

    Change port number to environment port number in server.py

--- server.py
+++ server.py
@@ -2,10 +2,12 @@ import os
 from flask import Flask, render_template
 
 app = Flask(__name__, static_folder='build/static', template_folder='build')
+port = int(os.environ.get("PORT", 5000))
 
 @app.route("/")
 def hello():
     return render_template('index.html')
 
+
 if __name__ == '__main__':
-    app.run(use_reloader=True, port=5000, threaded=True, debug=True)
+    app.run(host='0.0.0.0', port=port)
