bfc1d213eda80765c26181149593398536368fa2
Dingyu Chen <dingyuchowmein@yahoo.co.uk>
2019-10-12 13:11:44 +0100

    test auto CI

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React testtesttest
+          Learn React testtest
         </a>
       </header>
     </div>
