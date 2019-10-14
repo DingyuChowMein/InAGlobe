cf19e68f2957bb8138a4fc518f3795be0cac3628
darkchaoticlord <jawadadnan46@yahoo.com>
2019-10-14 18:44:12 +0100

    [ja2416]: Added other views that have been created for the WebApp.

---
+++ src/views/ProjectList/ProjectCard.js
@@ -0,0 +1,37 @@
+import React from "react"
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles"
+
+// core components
+import Button from "components/CustomButtons/Button.js"
+import Card from "components/Card/Card.js"
+import CardBody from "components/Card/CardBody.js"
+
+import styles from "assets/jss/material-dashboard-react/cardImagesStyles.js"
+
+const useStyles = makeStyles(styles)
+
+export default function ProjectCard(props) {
+    
+    const classes = useStyles()
+    const { title, organisation, status, description, image } = props
+    return (
+        <Card>
+            <img
+                className={classes.cardImgTop}
+                alt="Provided for a Card."
+                style={{ height: "180px", width: "100%", display: "block", objectFit: "cover" }}
+                src={image}
+                data-holder-rendered="true"
+            />
+            <CardBody>
+                <h3>{title}</h3>
+                <h4>{organisation}</h4>
+                <h5>{status}</h5>
+                <p>{description}</p>
+                <Button color="primary" style={{ marginRight: "10px"}}>{"Learn More"}</Button>
+                <Button color="primary">{"Select Proposal"}</Button>
+            </CardBody>
+        </Card>
+    )
+}
---
+++ src/views/ProjectList/ProjectList.js
@@ -0,0 +1,66 @@
+import React from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import ProjectCard from "views/ProjectList/ProjectCard"
+
+import pillPicture from "assets/img/card_example1.png"
+import smokePicture from "assets/img/card_example2.png"
+import emissionPicture from "assets/img/card_example3.png"
+import mobilePicture from "assets/img/card_example4.png"
+
+import styles from "assets/jss/material-dashboard-react/views/dashboardStyle.js"
+import GridContainer from "components/Grid/GridContainer"
+import GridItem from "components/Grid/GridItem"
+
+const useStyles = makeStyles(styles)
+
+const cardData = [
+    {
+        title: "Pill Organiser for Self-Medication",
+        organisation: "Fundaćion Mozambique Sur",
+        status: "Adopted (Department of Bioengineering)",
+        description: "A pill organiser for orphans in Casa do Gaiato-Maputo to self medicate appropriately. Motivated by the poor disease management due to limited number of healthcare workers.",
+        image: pillPicture
+    },
+    {
+        title: "Low-Smoke Cooking Environment",
+        organisation: "Helvetas Swiss Intercooperation",
+        status: "Adopted (Engineering Change)",
+        description: "Develop a low-cost stove that produced little or no smoke; or an efficient extractor system that can be coupled to current cooking methods.",
+        image: smokePicture
+    },
+    {
+        title: "Low Emission Cashew Nut Processor",
+        organisation: "Helvetas Swiss Intercooperation",
+        status: "Adopted (Engineering Change)",
+        description: "Develop a Cashew Nut processor with low CO2 emissions, as current technologies are highly polluting.",
+        image: emissionPicture
+    },
+    {
+        title: "Mobile Grain Network-Marketplace",
+        organisation: "Helvetas Swiss Intercooperation",
+        status: "Unadopted",
+        description: "Create a mobile phone application that informs of the fair local market price for different kinds of food productions.",
+        image: mobilePicture
+    }
+]
+
+export default function ProjectList() {
+    const classes = useStyles()
+    return (
+        <div className={classes.root}>
+            <GridContainer spacing={2}>
+                {cardData.map(card => (
+                    <GridItem xs={12}>
+                        <ProjectCard 
+                            title={card.title}
+                            organisation={card.organisation}
+                            status={card.status}
+                            description={card.description}
+                            image={card.image}
+                        />
+                    </GridItem>
+                ))}
+            </GridContainer>
+        </div>
+    )
+}
