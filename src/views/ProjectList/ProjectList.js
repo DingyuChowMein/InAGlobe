03ae1e4f430e8a3e75f3fdfe392a69f71da1f462
Harshil <harshils.how.to@gmail.com>
2019-10-17 16:45:38 +0100

    Converts view to class and sets up to use api call - awaiting endpoint to get projects to display

--- src/views/ProjectList/ProjectList.js
+++ src/views/ProjectList/ProjectList.js
@@ -11,56 +11,75 @@ import styles from "../../assets/jss/material-dashboard-react/views/dashboardSty
 import GridContainer from "../../components/Grid/GridContainer.js"
 import GridItem from "../../components/Grid/GridItem.js"
 
-const useStyles = makeStyles(styles)
 
-const cardData = [
-    {
-        title: "Pill Organiser for Self-Medication",
-        organisation: "Fundaćion Mozambique Sur",
-        status: "Adopted (Department of Bioengineering)",
-        description: "A pill organiser for orphans in Casa do Gaiato-Maputo to self medicate appropriately. Motivated by the poor disease management due to limited number of healthcare workers.",
-        image: pillPicture
-    },
-    {
-        title: "Low-Smoke Cooking Environment",
-        organisation: "Helvetas Swiss Intercooperation",
-        status: "Adopted (Engineering Change)",
-        description: "Develop a low-cost stove that produced little or no smoke; or an efficient extractor system that can be coupled to current cooking methods.",
-        image: smokePicture
-    },
-    {
-        title: "Low Emission Cashew Nut Processor",
-        organisation: "Helvetas Swiss Intercooperation",
-        status: "Adopted (Engineering Change)",
-        description: "Develop a Cashew Nut processor with low CO2 emissions, as current technologies are highly polluting.",
-        image: emissionPicture
-    },
-    {
-        title: "Mobile Grain Network-Marketplace",
-        organisation: "Helvetas Swiss Intercooperation",
-        status: "Unadopted",
-        description: "Create a mobile phone application that informs of the fair local market price for different kinds of food productions.",
-        image: mobilePicture
-    }
-]
+    const cardData = [
+        {
+            title: "Pill Organiser for Self-Medication",
+            organisation: "Fundaćion Mozambique Sur",
+            status: "Adopted (Department of Bioengineering)",
+            description: "A pill organiser for orphans in Casa do Gaiato-Maputo to self medicate appropriately. Motivated by the poor disease management due to limited number of healthcare workers.",
+            image: pillPicture
+        },
+        {
+            title: "Low-Smoke Cooking Environment",
+            organisation: "Helvetas Swiss Intercooperation",
+            status: "Adopted (Engineering Change)",
+            description: "Develop a low-cost stove that produced little or no smoke; or an efficient extractor system that can be coupled to current cooking methods.",
+            image: smokePicture
+        },
+        {
+            title: "Low Emission Cashew Nut Processor",
+            organisation: "Helvetas Swiss Intercooperation",
+            status: "Adopted (Engineering Change)",
+            description: "Develop a Cashew Nut processor with low CO2 emissions, as current technologies are highly polluting.",
+            image: emissionPicture
+        },
+        {
+            title: "Mobile Grain Network-Marketplace",
+            organisation: "Helvetas Swiss Intercooperation",
+            status: "Unadopted",
+            description: "Create a mobile phone application that informs of the fair local market price for different kinds of food productions.",
+            image: mobilePicture
+        }
+    ];
 
-export default function ProjectList() {
-    const classes = useStyles()
-    return (
-        <div className={classes.root}>
-            <GridContainer spacing={2}>
-                {cardData.map(card => (
-                    <GridItem xs={12} sm={12} md={6}>
-                        <ProjectCard 
-                            title={card.title}
-                            organisation={card.organisation}
-                            status={card.status}
-                            description={card.description}
-                            image={card.image}
-                        />
-                    </GridItem>
-                ))}
-            </GridContainer>
-        </div>
-    )
-}
+
+
+
+class ProjectList extends React.Component {
+
+     constructor(props) {
+            super(props);
+            this.state = {
+                projects: []
+            };
+        }
+
+     componentDidMount() {
+           fetch(`/api/users`)
+               .then(res => res.json())
+               .then(result => this.setState({ projects: result.projects }))
+       };
+
+
+        render() {
+            return (
+                    <GridContainer spacing={2}>
+                        {cardData.map(card => (
+                            <GridItem xs={12} sm={12} md={6}>
+                                <ProjectCard
+                                    title={card.title}
+                                    organisation={card.organisation}
+                                    status={card.status}
+                                    description={card.description}
+                                    image={card.image}
+                                />
+                            </GridItem>
+                        ))}
+                    </GridContainer>
+            );
+        }
+
+ }
+
+export default ProjectList;
