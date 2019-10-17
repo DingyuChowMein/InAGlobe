05ce73d3497f75e96d56598bf321a81ca50ec450
Dragos Martac <dmartac@gmail.com>
2019-10-17 12:38:10 +0100

    Supporting one file per project upload.

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ import sys
 import logging
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
+
 from config import SQLALCHEMY_DATABASE_URI
 from flask import request
 
@@ -35,6 +37,17 @@ def upload_project():
         location = db.Column(db.String, nullable=False)
         project_owner = db.Column(db.String, nullable=False)
 
+    class File(db.Model):
+        __tablename__ = 'Files'
+        __table_args__ = {'extend_existing': True}
+
+        file_id = db.Column(db.Integer, primary_key=True)
+        project_id = db.Column(db.Integer, ForeignKey(Project.project_id))
+        link = db.Column(db.String, nullable=False)
+
+    def parse_file_links(fileLinks):
+        return [fileLinks]
+
     parameters = ["title", "ShortDescription", "LongDescription", "Location", "ProjectOwner"]
     for parameter in parameters:
         if request.args.get(parameter) is None:
@@ -42,19 +55,31 @@ def upload_project():
         if not request.args[parameter].strip():
             return "<div>failure: the parameter named \"{}\" is empty</div>".format(parameter)
 
-    db.session.add(Project(
+    new_project = Project(
         title=request.args[parameters[0]],
         short_description=request.args[parameters[1]],
         long_description=request.args[parameters[2]],
         location=request.args[parameters[3]],
         project_owner=request.args[parameters[4]]
-    ))
+    )
+    db.session.add(new_project)
 
     try:
         db.session.commit()
     except Exception as e:
         return "<div>failure: {}</div>".format(str(e))
-    return "<div>success</div>"
+
+    fileLinks = request.args.get("FileLinks")
+    if fileLinks is not None:
+        links = parse_file_links(fileLinks)
+        for currLink in links:
+            db.session.add(File(project_id=new_project.project_id, link=currLink))
+
+    try:
+        db.session.commit()
+    except Exception as e:
+        return "<div>failure: {}</div>".format(str(e))
+    return "<div>success: added project with id {}</div>".format(new_project.project_id)
 
 
 if __name__ == '__main__':
