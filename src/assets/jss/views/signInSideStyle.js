224ca8c473f08670d7286c17cd49dbaf0e16f6ca
darkchaoticlord <jawadadnan46@yahoo.com>
2019-11-01 14:12:41 +0000

    [ja2416]: Have selected 10 specific pictures to work with the signin page.

--- src/assets/jss/views/signInSideStyle.js
+++ src/assets/jss/views/signInSideStyle.js
@@ -1,9 +1,22 @@
+const imageLinks = [
+	"https://picsum.photos/id/10/1920/1080",
+	"https://picsum.photos/id/1000/1920/1080",
+	"https://picsum.photos/id/1002/1920/1080",
+	"https://picsum.photos/id/1015/1920/1080",
+	"https://picsum.photos/id/1016/1920/1080",
+	"https://picsum.photos/id/1018/1920/1080",
+	"https://picsum.photos/id/1019/1920/1080",
+	"https://picsum.photos/id/1029/1920/1080",
+	"https://picsum.photos/id/1032/1920/1080",
+	"https://picsum.photos/id/1033/1920/1080"
+]
+
 const styles = theme => ({
 	root: {
 		height: '100vh',
 	},
 	image: {
-		backgroundImage: 'url(https://source.unsplash.com/random)',
+		backgroundImage: `url("${imageLinks[Math.floor(Math.random() * imageLinks.length)]}")`,
 		backgroundRepeat: 'no-repeat',
 		backgroundSize: 'cover',
 		backgroundPosition: 'center',
