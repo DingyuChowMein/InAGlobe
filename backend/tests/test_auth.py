f8f029eecfbf59bd84d29272d59742574b2b22a3
mikiivanovic <mi3917@ic.ac.uk>
2019-11-13 02:31:56 +0000

    Update test_auth.py

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -2,7 +2,6 @@ import pytest
 import sys
 
 sys.path.append('.')
-import json
 
 from src.models import User
 from conftests import db, app, auth, client
@@ -11,36 +10,40 @@ from conftests import db, app, auth, client
 ########################################################################################################################
 # User creation tests
 
-@pytest.mark.parametrize(('user_type'), (
-        ('ADMIN'),
-        ('HUMANITARIAN'),
-        ('ACADEMIC'),
-        ('STUDENT'),
+@pytest.mark.parametrize(('user_type', 'message', 'code'), (
+        ('ADMIN', b'Not allowed!', 403),
+        ('HUMANITARIAN', b'User created!', 201),
+        ('ACADEMIC', b'User created!', 201),
+        ('STUDENT', b'User created!', 201),
 ))
-def test_create_user(app, auth, user_type):
+def test_create_user(app, auth, user_type, message, code):
     with app.app_context():
         # Check that a user which has not been registered is not in the database
         assert (
-                db.session.query(User).filter(User.email == 'email').first()
+                db.session.query(User).filter(User.email == 'email@ic.ac.uk').first()
                 is None
         )
         # We now register the user
         rv = auth.create_user(user_type=user_type)
-        assert 201 == rv.status_code
-        assert b'User created!' in rv.data
+        assert code == rv.status_code
+        assert message in rv.data
         # Check the user has been registered in the database
         assert (
-                db.session.query(User).filter(User.email == 'email').first()
-                is not None
+            db.session.query(User).filter(User.email == 'email@ic.ac.uk').first()
+            is not None if code == 201 else
+            db.session.query(User).filter(User.email == 'email@ic.ac.uk').first()
+            is None
         )
 
 # TODO: error handling for bad user registration
 @pytest.mark.parametrize(('email', 'password', 'first_name', 'last_name', 'user_type', 'message', 'code'), (
-        # ('', 'password', 'name', 'surname', 'STUDENT', b'No email provided!', 400),
-        # ('email', '', 'name', 'surname', 'STUDENT', b'No password provided!', 400),
-        # ('email', 'password', '', 'surname', 'STUDENT', b'No name provided!', 400),
-        # ('email', 'password', 'name', '', 'STUDENT', b'No surname provided!', 400),
-        # ('email', 'password', 'name', 'surname', '', b'User type not specified!', 400),
+        ('', 'password', 'name', 'surname', 'STUDENT', b'Bad email provided!', 400),
+        ('', '', 'name', 'surname', 'STUDENT', b'Bad email provided!', 400),
+        ('email', '', 'name', 'surname', 'STUDENT', b'Bad password provided!', 400),
+        ('', 'tooshrt', 'name', 'surname', 'STUDENT', b'Bad email provided!', 400),
+        ('email', 'password', '', 'surname', 'STUDENT', b'Bad name provided!', 400),
+        ('email', 'password', 'name', '', 'STUDENT', b'Bad surname provided!', 400),
+        ('email', 'password', 'name', 'surname', '', b'Bad user type provided!', 400),
 ))
 def test_bad_create_user(auth, email, password, first_name, last_name, user_type, message, code):
     rv = auth.create_user(email, first_name, last_name, password, user_type)
@@ -56,34 +59,38 @@ def test_login(app, auth):
     with app.app_context():
         # A token is only generated after the user logs in, so it should not exist at this stage
         assert (
-            db.session.query(User).filter(User.email == 'email').first().token
+            db.session.query(User).filter(User.email == 'email@ic.ac.uk').first().token
             is None
         )
-        rv = auth.login('email', 'password')
+        rv = auth.login('email@ic.ac.uk', 'password')
         # The token should now exist
         assert (
-            db.session.query(User).filter(User.email == 'email').first().token
+            db.session.query(User).filter(User.email == 'email@ic.ac.uk').first().token
             is not None
         )
         assert 200 == rv.status_code
         assert b'token' in rv.data
 
 def test_login_multiple_users(auth):
-    auth.create_user(email='email1', first_name='one')
-    auth.create_user(email='email2', first_name='two')
-    auth.create_user(email='email3', first_name='three')
+    email1 = 'email1@ic.ac.uk'
+    email2 = 'email2@ic.ac.uk'
+    email3 = 'email3@ic.ac.uk'
 
-    rv = auth.login('email1', 'password')
+    auth.create_user(email=email1, first_name='one')
+    auth.create_user(email=email2, first_name='two')
+    auth.create_user(email=email3, first_name='three')
+
+    rv = auth.login(email1)
     assert b'one' in rv.data
     assert b'two' not in rv.data
     assert b'three' not in rv.data
 
-    rv = auth.login('email2', 'password')
+    rv = auth.login(email2)
     assert b'two' in rv.data
     assert b'one' not in rv.data
     assert b'three' not in rv.data
 
-    rv = auth.login('email3', 'password')
+    rv = auth.login(email3)
     assert b'three' in rv.data
     assert b'one' not in rv.data
     assert b'two' not in rv.data
@@ -91,8 +98,8 @@ def test_login_multiple_users(auth):
 
 # TODO: error handling for incorrect passwords
 @pytest.mark.parametrize(('email', 'password', 'message', 'code'), (
-        # ('email', 'wrongpassword', b'Incorrect password!', 401),
-        ('wrong_email', 'password', b'User does not exist!', 404),
+        ('email@ic.ac.uk', 'wrongpassword', b'Incorrect password!', 401),
+        ('wrong_email@ic.ac.uk', 'password', b'User does not exist!', 404),
 ))
 def test_bad_login(auth, email, password, message, code):
     auth.create_user()
@@ -106,15 +113,22 @@ def test_bad_login(auth, email, password, message, code):
 
 def test_logout(app, auth):
     with app.app_context():
-        auth.create_user()
+        # This user already exists in database
+        student_user = db.session.query(User).filter(User.email == 'student@ic.ac.uk').first()
+        # Token should not exist yet for the user
+        assert student_user.token is None
         # Get user token from login
-        rv = auth.login('email', 'password')
-        token = json.loads(rv.data.decode('utf-8')).get('token')
+        token = auth.get_token()
+        # Token should now exist
+        assert student_user.token is not None
+        assert User.check_token(token) == student_user
         # Test logout
-        rv_ = auth.logout(token)
-        assert 200 == rv_.status_code
-        assert b'User removed!' in rv_.data
+        rv = auth.logout(token)
+        assert 200 == rv.status_code
+        assert b'User removed!' in rv.data
         # Check that the token has expired
         assert User.check_token(token) is None
+        # However it has not been deleted
+        assert student_user.token is not None
 
 # TODO: add more 'bad' logout tests
