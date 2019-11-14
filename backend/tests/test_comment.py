496655c91d2d58b98f595389ad20fab716a24bf0
mikiivanovic <mi3917@ic.ac.uk>
2019-11-14 22:13:36 +0000

    Create test_comment.py
    initial stubs

---
+++ backend/tests/test_comment.py
@@ -0,0 +1,31 @@
+import pytest
+import sys
+
+sys.path.append('.')
+
+from src.models import Comment
+from conftests import db, app, auth, client
+from flask import g
+
+
+########################################################################################################################
+# Create comment tests
+
+def test_create_comment():
+
+    assert 0
+
+########################################################################################################################
+# Get comment tests
+
+def test_get_comment():
+
+    assert 0
+
+
+########################################################################################################################
+# Delete comment tests
+
+def test_delete_comment():
+
+    assert 0
