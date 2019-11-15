7fa21f076287ba59a61661965d7f279302daf81a
Dragos Martac <dmartac@gmail.com>
2019-11-15 14:02:26 +0000

    Basic user information displayed.

--- src/views/UserProfile/Profile.js
+++ src/views/UserProfile/Profile.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
-import { withStyles } from "@material-ui/core"
+import {withStyles} from "@material-ui/core"
 
 import ResponsiveDrawer from '../../components/ResponsiveDrawer/ResponsiveDrawer'
 
@@ -9,9 +9,29 @@ import styles from "../../assets/jss/views/profileStyle"
 class Profile extends Component {
 
     render() {
+        const user = JSON.parse(localStorage.getItem('user'))
+        let userType;
+        switch (user.permissions) {
+            case 0:
+                userType = "academic"
+                break
+            case 1:
+                userType = "humanitarian"
+                break
+            case 2:
+                userType = "academic"
+                break
+            case 3:
+                userType = "student"
+                break
+        }
+
         return (
             <ResponsiveDrawer name={"User Profile"}>
-                <h5>Hello World!</h5>
+                <h5><b>User Id</b> {user.userid}</h5>
+                <h5><b>User Type</b> {userType}</h5>
+                <h5><b>Name</b> {user.firstname + " " + user.lastname}</h5>
+                <h5><b>Email</b> {user.email}</h5>
             </ResponsiveDrawer>
         )
     }
