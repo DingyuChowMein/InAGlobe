1408996e643a89dc3c1b4ca0508dc09d6d5113a5
mikiivanovic <mi3917@ic.ac.uk>
2019-11-19 18:48:22 +0000

    Update SignInSide.js
    fix mutable error

--- src/views/SignInSide/SignInSide.js
+++ src/views/SignInSide/SignInSide.js
@@ -66,7 +66,8 @@ class SignInSide extends Component {
                     })
                 } else {
                     this.props.history.push("/main")
-                    this.state.loginFailed = false
+                    // this.state.loginFailed = false
+                    this.setState({loginFailed: false});
                 }
             })
             .catch(err => {
