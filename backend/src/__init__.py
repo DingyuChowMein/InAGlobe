d455a6ea25201c62314a84dc9ac95fbfd7308899
mikiivanovic <mi3917@ic.ac.uk>
2019-11-21 04:56:12 +0000

    Refactor
    move endpoint classes from factor to routes.py

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,23 +1,16 @@
-import logging
 import os
 
-from flask import Flask, request, make_response, Response
-from flask_cors import CORS, cross_origin
+from flask import Flask
+from flask_cors import CORS
 from flask_mail import Mail
-from flask_restful import Api, Resource
+from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from redis import Redis, StrictRedis
 
 # initialise sql-alchemy
 db = SQLAlchemy()
-red = StrictRedis()
+redis_client = StrictRedis()
 mail = Mail()
-
-
-# def new_response(json, status):
-#     response = make_response(json, status)
-#     response.headers.add("Access-Control-Allow-Origin", "*")
-#     return response
 
 
 def create_app():
@@ -29,143 +22,11 @@ def create_app():
     CORS(app)
     app.redis = Redis.from_url(app.config['REDIS_URL'])
 
-    from .projects import (get_projects, upload_project, approve_project, upload_checkpoint, get_dashboard_projects,
-                           select_project, get_joining_requests, approve_project_join, delete_project, update_project,
-                           project_stream)
-    from .users import (get_users, create_user, confirm_email, confirm_reset_password_token, reset_password,
-                        send_password_reset_email)
-    from .comments import add_comment, get_comments, delete_comment, comment_stream
-    from .tokens import get_token, revoke_token
-
-    # Define api
-    class Approvals(Resource):
-        def post(self):
-            response, code = approve_project(request.get_json())
-            return make_response(response, code)
-
-    class JoiningApproval(Resource):
-        def get(self):
-            response, code = get_joining_requests()
-            return make_response(response, code)
-
-        def post(self):
-            response, code = approve_project_join(request.get_json())
-            return make_response(response, code)
-
-    class Dashboard(Resource):
-        @cross_origin()
-        def get(self):
-            response, code = get_dashboard_projects()
-            return make_response(response, code)
-
-        def post(self):
-            response, code = select_project(request.get_json())
-            return make_response(response, code)
-
-    class Projects(Resource):
-        @cross_origin()
-        def get(self):
-            response, code = get_projects()
-            return make_response(response, code)
-
-        def post(self):
-            response, code = upload_project(request.get_json())
-            return make_response(response, code)
-
-        def delete(self, identifier):
-            response, code = delete_project(identifier)
-            return make_response(response, code)
-
-        def patch(self, identifier):
-            response, code = update_project(request.get_json(), identifier)
-            return make_response(response, code)
-
-    class ProjectStream(Resource):
-        def __init__(self):
-            # This works if a different projecttream instance is generated for each unique identifier
-            self.run_stream = True
-
-        @cross_origin()
-        def get(self, identifier):
-            app.logger.info('comment stream MARK')
-            return Response(project_stream(app, identifier, self.run_stream), mimetype='text/event-stream')
-
-        def delete(self):
-            # TODO handle closing the event stream
-            self.run_stream = False
-
-    class Comments(Resource):
-        @cross_origin()
-        def get(self, identifier):
-            response, code = get_comments(identifier)
-            app.logger.info('comment get')
-            return make_response(response, code)
-
-        def post(self, identifier):
-            response, code = add_comment(request.get_json(), identifier)
-            app.logger.info('comment post')
-            app.logger.info('publish to channel comment{}'.format(identifier))
-            return make_response(response, code)
-
-        def delete(self, identifier):
-            response, code = delete_comment(identifier)
-            app.logger.info('comment deleted')
-            return make_response(response, code)
-
-    class CommentStream(Resource):
-        def __init__(self):
-            # This works if a different commentStream instance is generated for each unique identifier
-            self.run_stream = True
-
-        @cross_origin()
-        def get(self, identifier):
-            app.logger.info('comment stream MARK')
-            return Response(comment_stream(app, identifier, self.run_stream), mimetype='text/event-stream')
-
-        def delete(self):
-            # TODO handle closing the event stream
-            self.run_stream = False
-
-    class Users(Resource):
-        def get(self):
-            response, code = get_users()
-            return make_response(response, code)
-
-        def post(self):
-            response, code = create_user(request.get_json())
-            return make_response(response, code)
-
-    class Checkpoints(Resource):
-        def post(self, identifier):
-            response, code = upload_checkpoint(request.get_json(), identifier)
-            return make_response(response, code)
-
-    class Tokens(Resource):
-        def get(self):
-            response, code = get_token()
-            return make_response(response, code)
-
-        def delete(self):
-            response, code = revoke_token()
-            return make_response(response, code)
-
-    class ConfirmEmail(Resource):
-        def get(self, token):
-            response, code = confirm_email(token)
-            return make_response(response, code)
-
-    class ResetPassword(Resource):
-        def get(self, token):
-            response, code = confirm_reset_password_token(token)
-            return make_response(response, code)
-
-        def post(self, token=None):
-            if token:
-                response, code = reset_password(token, request.get_json())
-                return make_response(response, code)
-
-            response, code = send_password_reset_email(request.get_json())
-            return make_response(response, code)
+    from .routes import (
+        Projects, ProjectStream,  Approvals, JoiningApproval, Dashboard, Checkpoints,
+        Comments, CommentStream,
+        Users, Tokens, ConfirmEmail, ResetPassword
+    )
 
     # Route classes to paths
     api.add_resource(Projects, '/projects/', '/projects/<int:identifier>/')
@@ -179,6 +40,6 @@ def create_app():
     api.add_resource(Dashboard, '/dashboard/')
     api.add_resource(Checkpoints, '/checkpoint/<int:identifier>/')
     api.add_resource(CommentStream, '/comment-stream/<int:identifier>')
-    api.add_resource(ProjectStream, '/project-stream/<int:identifier>')
+    api.add_resource(ProjectStream, '/project-stream/')
 
     return app
