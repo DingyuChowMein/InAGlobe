29d2ad5059c2bb100e8527fdbf1245866294359e
mikiivanovic <mi3917@ic.ac.uk>
2019-11-25 06:25:02 +0000

    Update ProjectCard.js
    refactor

--- src/views/ProjectList/ProjectCard.js
+++ src/views/ProjectList/ProjectCard.js
@@ -8,20 +8,18 @@ import {
     withStyles,
     Card,
     CardContent,
-    CardActions, IconButton, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button,
+    CardActions
 } from "@material-ui/core"
 
 // Imports of different components in project
 import RegularButton from "../../components/CustomButtons/RegularButton"
 
-import EditProject from "../../components/Projects/EditProjectModal"
+import ProjectDialogue from "../../components/Projects/EditProjectModal"
 
 import config from "../../config"
 
 // Import class's stylesheet
 import styles from "../../assets/jss/views/projectCardStyle"
-import {Close} from "@material-ui/icons";
-import {projectService} from "../../services/projectsService";
 
 class ProjectCard extends Component {
     constructor(props) {
@@ -33,83 +31,17 @@ class ProjectCard extends Component {
             deleteBox: false
         };
         this.openProposalPage = this.openProposalPage.bind(this);
-        this.deleteProject = this.deleteProject.bind(this);
-        this.renderWithPermissions = this.renderWithPermissions.bind(this);
-        this.renderConfirmDialog = this.renderConfirmDialog.bind(this);
+        this.hasPermission = this.hasPermission.bind(this);
     }
 
     openProposalPage() {
-        const dataValue = JSON.stringify(this.props.data)
-        localStorage.setItem(`proposalPage/${this.props.data.id}`, dataValue)
+        const dataValue = JSON.stringify(this.props.data);
+        localStorage.setItem(`proposalPage/${this.props.data.id}`, dataValue);
         this.props.history.push(`/main/projectlist/proposalpage/${this.props.data.id}`)
     }
 
-    deleteProject = (projectId) => {
-        projectService.deleteProject(projectId)
-            .then(response => {
-                console.log(response);
-            })
-            .catch(err => {
-                console.log(err)
-            })
-
-    };
-
-    renderWithPermissions = (ownerId) => {
-        if (this.state.userType === 0 || this.state.userId === ownerId) {
-              return (
-                  <>
-                      <EditProject ProjectData={this.props.data} />
-                      <IconButton onClick={() => {
-                                this.setState({
-                                    deleteBox: true
-                                })
-                            }}>
-                          <Close fontSize="medium"/>
-                      </IconButton>
-                  </>
-              )
-        }
-    };
-
-    renderConfirmDialog = () => {
-        return (
-            <Dialog
-                open={this.state.deleteBox}
-                onClose={() => {
-                    this.setState({deleteBox: false})
-                }}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <DialogTitle id="alert-dialog-title">Delete comment?</DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        Are you sure you want to delete this project?
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button
-                        onClick={() => {
-                            this.setState({deleteBox: false})
-                        }}
-                        color="primary"
-                    >
-                        No
-                    </Button>
-                    <Button
-                        onClick={() => {
-                            this.deleteProject(this.state.projectId);
-                            this.setState({deleteBox: false})
-                        }}
-                        color="primary"
-                        autoFocus
-                    >
-                        Yes
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        )
+    hasPermission = (ownerId) => {
+        return (this.state.userType === 0 || this.state.userId === ownerId)
     };
 
     render() {
@@ -142,9 +74,11 @@ class ProjectCard extends Component {
                     >
                         Learn More
                     </RegularButton>
-                    {this.renderWithPermissions(this.state.userId)}
+                    {this.hasPermission(this.state.userId) ?
+                        <ProjectDialogue ProjectData={this.props.data} /> :
+                        <></>
+                    }
                 </CardActions>
-                {this.renderConfirmDialog()}
             </Card>
 
     )
